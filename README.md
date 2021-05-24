e8d39ee14e49e81dc95dc0632413f4f4f9835ede
Kamil Stawik <kamil_stawik@op.pl>
2021-05-24 19:09:28 +0200

    BEM fixed / README added

---
+++ README.md
@@ -0,0 +1,22 @@
+# TO-DO-LIST
+
+## Demo
+
+[To-do-list by Kamil](https://kamilstawik.github.io/to-do-list/)
+
+![Project screenshot1](/images/screenshot1.jpg)
+
+## About me
+
+Hi! My name is Kamil. At the begining of 2021 I had a briliant idea. I started to lern programming. In future i would like to become a Frontend Developer.
+
+## About this project
+
+This is my third study project. Very simple task list. I applied basic capabilities adding / removing and marking tasks as done.
+
+## Technologies and conventions used in project:
+* HTML
+* CSS
+* javascript
+* BEM
+* CamelCase
--- js/script.js
+++ js/script.js
@@ -74,10 +74,10 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li class =\"listItem\"">
-                <button class="js-done doneButton">${task.done ? "✓" : ""}</button>
-                <span ${task.done ? " class =\"taskContent taskContent--done\"" : " class =\"taskContent\""}>${task.content}</span>
-                <button class="js-remove removeButton">🗑</button>
+                <li class =\"taskListContainer__listItem\"">
+                <button class="js-done taskListContainer__doneButton">${task.done ? "✓" : ""}</button>
+                <span ${task.done ? " class =\"taskListContainer__taskContent taskListContainer__taskContent--done\"" : " class =\"taskListContainer__taskContent\""}>${task.content}</span>
+                <button class="js-remove taskListContainer__removeButton">🗑</button>
                 </li>
             `;
         }
@@ -94,102 +94,8 @@
 
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", onFormSubmit);
-
-
     };
-
-
-
 
     init();
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ORYGINALNY KALKULATOR WALUT!
-// {
-//     const currencyElement = document.querySelector(".js-currency");
-//     const resultCurrencyElement = document.querySelector(".js-resultCurrency");
-
-
-//     const checkCurrencyPairRate = () => {
-
-//         const currencyWorthEURPLN = 4.57;
-//         const currencyWorthPLNEUR = 1 / currencyWorthEURPLN;
-
-//         const currencyWorthUSDPLN = 3.77;
-//         const currencyWorthPLNUSD = 1 / currencyWorthUSDPLN;
-
-//         const currencyWorthEURUSD = 1.21;
-//         const currencyWorthUSDEUR = 1 / currencyWorthEURUSD;
-
-//         if (currencyElement.value === "PLN" && resultCurrencyElement.value === "EUR") {
-//             return (currencyWorthPLNEUR);
-
-//         } else if (currencyElement.value === "EUR" && resultCurrencyElement.value === "PLN") {
-//             return (currencyWorthEURPLN);
-
-//         } else if (currencyElement.value === "PLN" && resultCurrencyElement.value === "USD") {
-//             return (currencyWorthPLNUSD);
-
-//         } else if (currencyElement.value === "EUR" && resultCurrencyElement.value === "USD") {
-//             return (currencyWorthEURUSD);
-
-//         } else if (currencyElement.value === "USD" && resultCurrencyElement.value === "PLN") {
-//             return (currencyWorthUSDPLN);
-
-//         } else if (currencyElement.value === "USD" && resultCurrencyElement.value === "EUR") {
-//             return (currencyWorthUSDEUR);
-
-//         } else if (currencyElement.value === resultCurrencyElement.value) {
-//             currencyElement.classList.add("form__selectField--error");
-//             resultCurrencyElement.classList.add("form__selectField--error");
-//         }
-//         else {
-//             console.log("error");
-//         }
-//     }
-
-//     const calculateResult = () => {
-//         const amountElement = document.querySelector(".js-amount");
-//         return (amountElement.value * checkCurrencyPairRate()).toFixed(2);
-
-//     }
-
-//     const removeErrors = () => {
-//         currencyElement.classList.remove("form__selectField--error");
-//         resultCurrencyElement.classList.remove("form__selectField--error");
-//     }
-
-
-//     const init = () => {
-
-//         const formElement = document.querySelector(".js-form");
-//         const resultElement = document.querySelector(".js-result");
-
-//         formElement.addEventListener("submit", (event) => {
-//             event.preventDefault();
-//             removeErrors();
-//             resultElement.value = calculateResult();
-//         })
-//     }
-
-//     init();
-// }
